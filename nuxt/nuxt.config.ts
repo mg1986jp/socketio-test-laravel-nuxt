2ef1f02992d1d2df59a05bcabb678a2e6c4f0fc1
MasayaGoto <mg1986jp@gmail.com>
2025-12-19 03:09:02 +0900

    chore: recreate nuxt via official template and bump php to 8.4

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,11 +1,5 @@
-// Minimal Nuxt 3 config with Vuetify
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: [
-    'vuetify/styles',
-    '@mdi/font/css/materialdesignicons.min.css',
-  ],
-  build: {
-    transpile: ['vuetify'],
-  },
-});
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
