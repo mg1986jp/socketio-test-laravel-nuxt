51edd79246b5de4711af1085e5fa43d52f1db5b3
MasayaGoto <mg1986jp@gmail.com>
2025-12-19 11:34:42 +0900

    oauth_device_codesにFKとrevokedデフォルトを追加

--- laravel/database/migrations/2025_12_19_040105_create_oauth_device_codes_table.php
+++ laravel/database/migrations/2025_12_19_040105_create_oauth_device_codes_table.php
@@ -17,10 +17,19 @@ return new class extends Migration
             $table->foreignUuid('client_id')->index();
             $table->char('user_code', 8)->unique();
             $table->text('scopes');
-            $table->boolean('revoked');
+            $table->boolean('revoked')->default(false);
             $table->dateTime('user_approved_at')->nullable();
             $table->dateTime('last_polled_at')->nullable();
             $table->dateTime('expires_at')->nullable();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreign('client_id')
+                ->references('id')
+                ->on('oauth_clients')
+                ->onDelete('cascade');
         });
     }
 
